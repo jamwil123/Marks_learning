a305c83f40779ad061e6b989ac5dc9148a1edcf7
James <james789111@gmail.com>
2024-08-19 18:19:58 +0100

    changes

---
+++ temp.js
@@ -0,0 +1,9 @@
+let age = 10;
+let height = 1.75;
+let name = "James";
+let isStudent = false;
+
+console.log(`My name is ${name}.`);
+console.log(`I am ${age} years old`);
+console.log(`My height is ${height} metres.`);
+console.log(`Am I a student? ${isStudent}`);
